7411dfb2a6a21d40bd776fe882b76cffabd109e9
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-23 22:58:52 +0100

    feat: user login page

--- app/Http/Controllers/Auth/UserLoginController.php
+++ app/Http/Controllers/Auth/UserLoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class UserLoginController extends Controller
 {
