19c203283c373757c372616dfd21a00bd6163885
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-23 21:41:46 +0100

    feat: register user auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\UserLoginController;
+use App\Http\Controllers\Auth\UserRegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [UserLoginController::class, "show"])->name("login-user");
+Route::post('/login', [UserLoginController::class, 'store']);
+Route::get('register', [UserRegisterController::class, 'show'])->name('register-user');
+Route::post('register', [UserRegisterController::class, 'store']);
