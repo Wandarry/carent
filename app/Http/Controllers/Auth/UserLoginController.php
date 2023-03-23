02ba5a53646524a8d3b240cd60190a6fb2c99f03
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-23 21:33:29 +0100

    feat: add user registration

--- app/Http/Controllers/Auth/UserLoginController.php
+++ app/Http/Controllers/Auth/UserLoginController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
-use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserLoginController extends Controller
 {
