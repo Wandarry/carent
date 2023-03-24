e7d5d61374111b85ec5f941623f536807243f358
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-24 12:40:33 +0100

    feat: add admin login

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminLoginController extends Controller
 {
-    //
+    public function show() {
+        return view("admin.auth.login");
+    }
+
+    public function store(LoginRequest $request) {
+        $authData = $request->only(['email', 'password']);
+
+        if (Auth::attempt($authData))
+        {
+            Auth::login(Auth::user(), true);
+
+            return redirect("/home");
+        }
+        return back()->withErrors(["error" => "Bad credentials"]);
+    }
 }
