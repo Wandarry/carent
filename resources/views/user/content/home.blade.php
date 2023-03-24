8f4aa1a211ed1de90556d9bf427e62c28a7256bb
Wandarry <berryenick20@gmail.com>
2023-03-24 23:48:04 +0100

    Landing page

--- resources/views/user/content/home.blade.php
+++ resources/views/user/content/home.blade.php
@@ -15,4 +15,30 @@
             </section>
         </div>
     </div>
+
+    <div class="py-10 px-9 flew items-center justify-center">
+        <div>
+            <p class="text-orange-400 font-bold text-4xl tracking-widest">Who we are ?</p>
+            <p class="py-6">CARENT is a premier car rental company that offers a wide range of vehicles to meet the transportation needs of individuals, families, and businesses. With a fleet of well-maintained cars, SUVs, and vans, we offer flexible rental plans that suit different budgets and preferences. Our mission is to provide our customers with high-quality cars and exceptional customer service, making their rental experience hassle-free and enjoyable. Our team of trained and experienced professionals is dedicated to ensuring that each customer receives personalized attention and support throughout their rental journey.</p>
+        </div>
+    </div>
+
+    <div class="py-4 px-9 flew items-center justify-center">
+        <p class="text-orange-400 font-bold text-4xl tracking-widest">Most rent cars</p>
+        <div class="py-2 flex justify-center gap-8">
+            <div class="w-1/4 h-300">
+                <img src="./images/7046342_1593.jpg" alt="">
+            </div>
+            <div class="w-1/4 h-300">
+                <img src="./images/blue-sport-sedan-parked-yard.jpg" alt="">
+            </div>
+            <div class="w-1/4 h-300">
+                <img src="./images/isolated-white-van-white-surface.jpg" alt="">
+            </div>
+            <div class="w-1/4 h-300">
+                <img src="./images/red-luxury-sedan-road.jpg" alt="">
+            </div>
+        </div>
+    </div>
 @endsection
+
